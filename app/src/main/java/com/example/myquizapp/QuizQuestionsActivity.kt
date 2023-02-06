46d6e610dc033ef58e991a29b3806d8bc5c6e0a9
EserYueksel <eseryuksel@live.de>
2023-02-06 08:58:50 +0100

    - correct and wrong answer colors added to buttons

--- app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
@@ -6,10 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
 import org.w3c.dom.Text
 
@@ -133,8 +130,65 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.btn_submit ->{
-                // TODO "implement btn submit"
+                if(mSelectedOptionPosition == 0){
+                    mCurrentPosition++
+
+                    when{
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }
+                        else ->{
+                            Toast.makeText(this,"You made it to the end",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    // If selected answer is incorrect, the selected red color will be shown as the button bg
+                } else {
+                    val question = mQuestionsList?.get(mCurrentPosition -1)
+                    if(question!!.correctAnswer != mSelectedOptionPosition){
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+                    // similar here, only for the correct answer and green bg color for button
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if(mCurrentPosition == mQuestionsList!!.size){
+                        btnSubmit?.text = "FINISH"
+                    }else{
+                        btnSubmit?.text = "GO TO NEXT QUESTION"
+                    }
+                    // Set back the selected option to zero in order to start every question unselected
+                    mSelectedOptionPosition = 0
+
+                }
             }
+        }
+    }
+
+    private fun answerView(answer: Int, drawableView: Int){
+        when(answer){
+            1 -> {
+                tvOptionOne?.background = ContextCompat.getDrawable(
+                    this,
+                    drawableView
+                )
+            }
+            2 -> {
+            tvOptionTwo?.background = ContextCompat.getDrawable(
+                this,
+                drawableView
+            )
+        }
+            3 -> {
+            tvOptionThree?.background = ContextCompat.getDrawable(
+                this,
+                drawableView
+            )
+        }
+            4 -> {
+            tvOptionFour?.background = ContextCompat.getDrawable(
+                this,
+                drawableView
+            )
+        }
         }
     }
 }
